2775d628f8448c634375900c78577851b8dcb327
ChristianLoza <christianlzap@gmail.com>
2021-11-17 11:32:35 -0500

    implementation

--- app/src/main/java/edu/uniandes/vinilosapp/MainActivity.kt
+++ app/src/main/java/edu/uniandes/vinilosapp/MainActivity.kt
@@ -2,10 +2,14 @@ package edu.uniandes.vinilosapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.uniandes.vinilosapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
