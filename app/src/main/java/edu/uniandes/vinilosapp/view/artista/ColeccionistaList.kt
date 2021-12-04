b7e55a109ca7a30cb810a11d64f29ee31922c18f
Félix Acero <acerofel@uniandes.edu.co>
2021-12-03 21:07:45 -0500

    Mejoras
    Mejoras del ViewModel

--- app/src/main/java/edu/uniandes/vinilosapp/view/artista/ColeccionistaList.kt
+++ app/src/main/java/edu/uniandes/vinilosapp/view/artista/ColeccionistaList.kt
@@ -1,17 +1,18 @@
 package edu.uniandes.vinilosapp.view.artista
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import edu.uniandes.vinilosapp.adapter.ColeccionistaAdapter
 import edu.uniandes.vinilosapp.databinding.ActivityColeccionistaListBinding
-import edu.uniandes.vinilosapp.util.ViewModelFactory
 import edu.uniandes.vinilosapp.viewmodel.ColeccionistaViewModel
 
 class ColeccionistaList : AppCompatActivity() {
 
     private lateinit var binding: ActivityColeccionistaListBinding
+
+    private val viewModel:ColeccionistaViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +20,6 @@ class ColeccionistaList : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.coleccionistaListRecycler.layoutManager = LinearLayoutManager(this)
-
-        val viewModel = ViewModelProvider(this,ViewModelFactory(application)).get(ColeccionistaViewModel::class.java)
 
         val adapter = ColeccionistaAdapter()
         binding.coleccionistaListRecycler.adapter = adapter
